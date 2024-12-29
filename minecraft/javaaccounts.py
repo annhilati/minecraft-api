b7ef24362da64beaff974213dd4ef6eaabf619d4
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-12-29 12:31:03 +0100

    Update javaaccounts.py

--- minecraft/javaaccounts.py
+++ minecraft/javaaccounts.py
@@ -1,27 +1,29 @@
 from .api import Endpoints
 
 class JavaAccount():
-    """
-    Class-Object for fecthing information about an Minecraft Java Edition account
-
-    #### Arguments:
-        identifier (str): The accounts UUID or current playername
-
-    #### Attributes:
-        uuid (str): The accounts Unique Universal Identifier
-        playername(str): The accounts current playername
-    
-    #### Methods:
-        update: Updates all attributes of the JavaAccount object that may change over time
-    """
 
     def __init__(self, identifier: str):
-        if len(identifier) in [32, 36]:
-            uuid = identifier.replace("-", "")
-            playername = Endpoints.SESSIONSERVER_PROFILE.fetch(uuid=uuid)["name"]
-        else:
+        """
+        Class-Object for fecthing information about an Minecraft Java Edition account
+
+        #### Arguments:
+            identifier (str): The accounts UUID or current playername
+
+        #### Attributes:
+            uuid (str): The accounts Unique Universal Identifier
+            playername(str): The accounts current playername
+        
+        #### Methods:
+            update: Updates all attributes of the JavaAccount object that may change over time
+        """
+            
+        if len(identifier) not in [32, 36]:
             playername = identifier
             uuid = Endpoints.MOJANG_PROFILE.fetch(playername=playername)["id"]
+        else:
+            uuid = identifier.replace("-", "")
+
+        self.update() # Lädt somit alle anderen Eigenschaften initial
 
         self.uuid = uuid
         self.playername = playername
